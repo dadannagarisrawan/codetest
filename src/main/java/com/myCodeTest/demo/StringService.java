e3de5fb325c24760c48bdb3b9c17c14dacc752fa
Srawan Dadannagari <sd.javapro0715@gmail.com>
2018-05-29 11:29:55 -0700

    Adding service class

--- src/main/java/com/myCodeTest/demo/StringService.java
+++ src/main/java/com/myCodeTest/demo/StringService.java
@@ -5,38 +5,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class StringService {
 
-
-    public boolean isCharacterArray(char[] c) {
-        int count = 0;
-        for (int i = 0; i < c.length; i++) {
-            if (!Character.isLetter(c[i])) {
-                count++;
-            }
-        }
-
-        if (count > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     public String caseReversal(String input) {
-        if (input != null && !input.isEmpty()) {
+        if ((input != null && !input.isEmpty()) && input.matches("^.[^a-zA-Z ].$")) {
             char[] charArray = input.toCharArray();
-
-  //          if (isCharacterArray(charArray)) {
-                for (int i = 0; i < charArray.length; i++) {
-                    if (Character.isLowerCase(charArray[i])) {
-                        charArray[i] = Character.toUpperCase(charArray[i]);
-                    } else {
-                        charArray[i] = Character.toLowerCase(charArray[i]);
-                    }
+            for (int i = 0; i < charArray.length; i++) {
+                if (Character.isLowerCase(charArray[i])) {
+                    charArray[i] = Character.toUpperCase(charArray[i]);
+                } else {
+                    charArray[i] = Character.toLowerCase(charArray[i]);
                 }
-   //         }
+            }
             return new String(charArray);
         } else {
             return "Invalid character in input string";
         }
     }
+
 }
